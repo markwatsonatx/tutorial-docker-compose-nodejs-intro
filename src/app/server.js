612884d11b0a8768d6cd11f279538c14e46e3313
Mark Watson <mark.watson.x@gmail.com>
2016-06-16 00:14:00 -0500

    Initial commit

---
+++ src/app/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+// serve static files from ./public
+app.use(express.static(__dirname + '/public'));
+
+// start server
+app.listen(config.express.port, '0.0.0.0', function() {
+  console.log('Server started on port ' + config.express.port + '.')
+});
